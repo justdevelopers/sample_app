5ba0c47882c6d98821d51e9b98ddd89eaf555b31
simplydesigner <simplydesigner@jd.ru>
2015-09-24 05:17:30 +0300

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -113,4 +113,19 @@ describe User do
     before { @user.save }
     it { expect(@user.remember_token).not_to be_blank }
   end
+
+  it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
+
+  it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 end
