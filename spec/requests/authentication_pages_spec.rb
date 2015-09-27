cbe249a53c046114db3350e2e2efd47b91b88f99
simplydesigner <simplydesigner@jd.ru>
2015-09-27 08:06:43 +0300

    Refactored

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -10,6 +10,9 @@ describe "Authentication" do
     it { should have_content('Sign in') }
     it { should have_title('Sign in') }
 
+    it { should_not have_link('Profile') }
+    it { should_not have_link('Settings') }
+
     describe "with invalid information" do
       before { click_button "Sign in" }
 
@@ -42,6 +45,25 @@ describe "Authentication" do
 
   describe "authorization" do
 
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before { sign_in user, no_capybara: true }
+
+      describe "in the Users controller" do
+
+        describe "visiting the new page" do
+          before { get new_user_path }
+          specify { expect(response).to redirect_to(root_url) }
+        end
+
+        describe "submitting to the create action" do
+          before { post users_path, user: FactoryGirl.attributes_for(:user) }
+          specify { expect(response).to redirect_to(root_url) }
+        end
+      end
+
+    end
+
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
@@ -66,15 +88,26 @@ describe "Authentication" do
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
-          fill_in "Email",    with: user.email
-          fill_in "Password", with: user.password
-          click_button "Sign in"
+          sign_in user
         end
 
         describe "after signing in" do
-
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
+          end
+
+          describe "when signing in again" do
+            before do
+              delete signout_path
+              visit signin_path
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+
+            it "should render the default (profile) page" do
+              expect(page).to have_title(user.name)
+            end
           end
         end
       end
@@ -91,5 +124,23 @@ describe "Authentication" do
         specify { expect(response).to redirect_to(root_url) }
       end
     end
+
+    describe "as admin user" do
+      let(:admin) { FactoryGirl.create(:admin) }
+      before { sign_in admin, no_capybara: true }
+    
+      describe "submitting a DELETE request to the Users#destroy action self" do
+        let(:response) { delete user_path(admin) }
+
+        it 'should not change user count' do
+            expect{ response }.to_not change(User, :count)
+        end
+
+        it 'should redirect to root_url' do
+            expect(response).to redirect_to(root_url)
+        end
+      end
+    end
+
   end
 end
